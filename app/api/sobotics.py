10a340aefd95157f900db24831612732cb4089c6
Filnor <philipp.bergamin@gmail.com>
2018-11-27 10:35:01 +0100

    Remove API method for scoreboard updates

--- app/api/sobotics.py
+++ app/api/sobotics.py
@@ -20,12 +20,12 @@ def scoreboard():
 
     scoreboard_map = []
 
-    # doc = db.collection(u"scores").document(u"sobotics")
-
     #Load data
     docs = db.collection(u"scores").get()
     for doc in docs:
         score = doc.to_dict()
+        if int(score["flag_count"]) <= 499:
+            continue
         user_name_and_profile_link = _get_user_name_and_profile_link(score["user_id"])
         scoreboard_map.append({"profile_link": user_name_and_profile_link[1], "username": user_name_and_profile_link[0], "flag_count": score["flag_count"]})
 
@@ -33,41 +33,6 @@ def scoreboard():
     scoreboard_map = sorted(scoreboard_map, key=itemgetter("flag_count"))
 
     response = jsonify(scoreboard_map)
-    response.status_code = 200
-    return response
-
-
-@bp.route('/scoreboard/add', methods=['POST'])
-def add_scoreboard():
-    data = request.get_json()
-
-    if data is None or data["fkey"] != Config().fkey:
-        return ex.Forbidden()
-    existing_record = False
-    doc_id = None
-
-    # Skip if flag count is unchanged
-    docs = db.collection(u"scores").get()
-    for doc in docs:
-        db_data = doc.to_dict()
-        if data['user_id'] == db_data['user_id']:
-            existing_record = True
-            if data['flag_count'] == db_data['flag_count']:
-                response = jsonify({"message": "Flag count unchanged. Nothing was changed."})
-                response.status_code = 200
-                return response
-            else:
-                doc_id = doc.id
-                break
-
-    # Update record
-    if existing_record:
-        db.collection(u"scores").document(doc_id).update({u"user_id": data["user_id"], u"flag_count": data["flag_count"]})
-    # Add new record
-    elif not existing_record:
-        db.collection(u"scores").add({u"user_id": data["user_id"], u"flag_count": data["flag_count"]})
-
-    response = jsonify({"message": f"Scoreboard entry {'added' if not existing_record else 'updated'}."})
     response.status_code = 200
     return response
 
