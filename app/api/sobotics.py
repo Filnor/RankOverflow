684b5c7a53ad3a4ef9ba59342d49a52c9a2a6609
Filnor <philipp.bergamin@gmail.com>
2019-11-20 01:44:49 +0100

    Server-side scoreboard generation closes #12

--- app/api/sobotics.py
+++ app/api/sobotics.py
@@ -11,29 +11,6 @@ from app.models import db
 from app.models.Score import Score
 import werkzeug.exceptions as ex
 
-
-@bp.route('/scoreboard', methods=['POST'])
-def get_scoreboard():
-    data = request.get_json()
-
-    if data is None or data["fkey"] != Config().fkey:
-        return ex.Forbidden()
-
-    objects = Score.query.all()
-
-    scores = []
-    
-    for obj in objects:
-        name_profile_link = _get_user_name_and_profile_link(obj.id)
-        scores.append({"profile_link": name_profile_link[1], "username": name_profile_link[0], "flag_count": obj.flags})
-
-    #Sort data
-    scores = sorted(scores, key=itemgetter("flag_count"), reverse=True)
-
-    response = jsonify(scores)
-    response.status_code = 200
-    return response
-
 @bp.route('/scoreboard/store', methods=['POST'])
 def store_entry():
     data = request.get_json()
@@ -56,18 +33,3 @@ def store_entry():
     response = jsonify(return_entry.as_dict())
     response.status_code = 200
     return response
-
-def _get_user_name_and_profile_link(user_id):
-    """
-    Get an user object for the specified user id from the API
-    """
-    try:
-        response = urlopen(
-            f"https://api.stackexchange.com/2.2/users/{user_id}?order=desc&sort=reputation&site=stackoverflow&key=K8pani4F)SeUn0QlbHQsbA((").read()
-        buffer = io.BytesIO(response)
-        gzipped_file = gzip.GzipFile(fileobj=buffer)
-        content = gzipped_file.read()
-        users = json.loads(content.decode("utf-8"))
-        return users["items"][0]["display_name"], users["items"][0]["link"]
-    except urllib.error.HTTPError as e:
-        return None
--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,27 @@ from datetime import date
 from flask import render_template
 from app import app
 from config import Config
+from app.models.Score import Score
+from urllib.request import urlopen
+import io
+import gzip
+import json
+import urllib
+from operator import itemgetter
+
+def _get_user_name_and_profile_link(user_id):
+    """
+    Get an user object for the specified user id from the API
+    """
+    try:
+        response = urlopen(f"https://api.stackexchange.com/2.2/users/{user_id}?order=desc&sort=reputation&site=stackoverflow&key=K8pani4F)SeUn0QlbHQsbA((").read()
+        buffer = io.BytesIO(response)
+        gzipped_file = gzip.GzipFile(fileobj=buffer)
+        content = gzipped_file.read()
+        users = json.loads(content.decode("utf-8"))
+        return users["items"][0]["display_name"], users["items"][0]["link"]
+    except urllib.error.HTTPError as e:
+        return None
 
 @app.route('/')
 @app.route('/index')
@@ -11,8 +32,17 @@ def index():
 
 @app.route('/scoreboard')
 def scoreboard():
-    current_year = date.today().year
-    return render_template('scoreboard.html', fkey=Config().fkey)
+    objects = Score.query.all()
+
+    scores = []
+    
+    for obj in objects:
+        name_profile_link = _get_user_name_and_profile_link(obj.id)
+        scores.append({"profile_link": name_profile_link[1], "username": name_profile_link[0], "flag_count": obj.flags})
+
+    #Sort data
+    scores = sorted(scores, key=itemgetter("flag_count"), reverse=True)
+    return render_template('scoreboard.html', scores=scores)
 
 @app.route('/blank')
 def blank():
