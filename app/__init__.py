71a04d6fb1a4841c5f3449f8c5bf62df92b32079
Filnor <philipp.bergamin@gmail.com>
2020-03-12 10:19:44 +0000

    Upload server status

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from config import Config
-from app.models.__init__ import db
+#from app.models.__init__ import db
 from flask_migrate import Migrate
 from app.models.Score import Score
 from app.models.CustomRank import CustomRank
@@ -11,6 +11,9 @@ app.config.from_object(Config)
 from app.api import bp as api_bp
 app.register_blueprint(api_bp, url_prefix='/api')
 
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 db.init_app(app)
 migrate = Migrate(app, db)
 
--- app/api/sobotics.py
+++ app/api/sobotics.py
@@ -18,18 +18,27 @@ def store_entry():
     if data is None or data["fkey"] != Config().fkey:
         return ex.Forbidden()
 
-    return_entry = {}
-    if len(Score.query.filter_by(id=data["id"]).all()) == 1:
-        existing_score = Score.query.filter_by(id=data["id"]).first()
-        existing_score.flags = data["flags"]
-        db.engine.execute("UPDATE score SET flags = '{}' WHERE id = {}".format(data["flags"], data["id"]))
-        return_entry = existing_score
-    else:
-        scoreboard_entry = Score(id=data["id"], flags=data["flags"])
-        return_entry = scoreboard_entry
-        db.session.add(scoreboard_entry)
-        db.session.commit()
-
-    response = jsonify(return_entry.as_dict())
-    response.status_code = 200
-    return response
+    response = None
+    db.session.close()
+    #db.dispose()
+    try:
+        return_entry = {}
+        if len(Score.query.filter_by(id=data["id"]).all()) == 1:
+            existing_score = Score.query.filter_by(id=data["id"]).first()
+            existing_score.flags = data["flags"]
+            db.engine.execute("UPDATE score SET flags = '{}' WHERE id = {}".format(data["flags"], data["id"]))
+            return_entry = existing_score
+        else:
+            scoreboard_entry = Score(id=data["id"], flags=data["flags"])
+            return_entry = scoreboard_entry
+            db.session.add(scoreboard_entry)
+            db.session.commit() 
+        response = jsonify(return_entry.as_dict())
+        response.status_code = 200
+    except BaseException:
+        db.session.rollback()
+        response.status_code = 503
+    finally:
+       db.session.close()
+       #db.dispose()
+       return response
